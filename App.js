75cccdf1d681a78b39faf618abc78ff7e2b39832
ViniciusFrancaCroa46 <viniciusfrancacoviello@mail.com>
2025-04-01 22:17:34 -0300

    Integração com API Spring Boot

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import AppNavigator from './src/navigation/AppNavigator';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <AppNavigator />
+    </PaperProvider>
   );
 }
 
